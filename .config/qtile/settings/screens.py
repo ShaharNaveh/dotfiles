c62123976cd0a302d60d4c5f902e9eedf684a1e9
ShaharNaveh <>
2020-12-14 21:43:26 +0200

    Removed commented code

--- .config/qtile/settings/screens.py
+++ .config/qtile/settings/screens.py
@@ -1,7 +1,6 @@
 from libqtile import bar
 from libqtile.config import Screen
 
-# from settings.widgets import secondary_widgets
 from settings.widgets import primary_widgets
 
 
@@ -10,22 +9,3 @@ def status_bar(widgets):
 
 
 screens = [Screen(top=status_bar(primary_widgets))]
-
-
-"""
-# For multi monitor support
-connected_monitors = (
-    subprocess.run(
-        "xrandr | grep 'connected' | cut -d ' ' -f 2",
-        shell=True,
-        stdout=subprocess.PIPE,
-    )
-    .stdout.decode("UTF-8")
-    .split("\n")[:-1]
-    .count("connected")
-)
-
-if connected_monitors > 1:
-    for _ in range(1, connected_monitors):
-        screens.append(Screen(top=status_bar(secondary_widgets)))
-"""
