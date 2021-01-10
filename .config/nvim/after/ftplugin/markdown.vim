723b94de0c21326df13fb194ca90cd1f567b41e2
ShaharNaveh <>
2021-01-10 21:49:46 +0200

    Save

--- .config/nvim/after/ftplugin/markdown.vim
+++ .config/nvim/after/ftplugin/markdown.vim
@@ -1,9 +1,8 @@
 setlocal spell spelllang=en_us
 setlocal cursorline
-setlocal conceallevel=3
+setlocal conceallevel=2
 setlocal linebreak
 setlocal scrolloff=12
 setlocal mouse="nvi"
-setlocal nocursorline
 setlocal nocursorcolumn
 setlocal wrap
--- .config/qtile/settings/screens.py
+++ .config/qtile/settings/screens.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from libqtile import bar
 from libqtile.config import Screen
 
@@ -9,3 +11,14 @@ def status_bar(widgets):
 
 
 screens = [Screen(top=status_bar(primary_widgets))]
+
+connected_monitors = (
+    subprocess.run(
+        "xrandr | grep 'connected' | cut -d ' ' -f 2",
+        shell=True,
+        stdout=subprocess.PIPE,
+    )
+    .stdout.decode("UTF-8")
+    .split("\n")[:-1]
+    .count("connected")
+)
