8fcec3e00bf652be5066facc4530515f4a140604
ShaharNaveh <>
2021-01-16 15:45:58 +0200

    Reorgenize aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,19 +15,11 @@ if command -s bat > /dev/null
 	alias cat="bat"
 end
 
-alias cleanup='sudo pacman -Rns (pacman -Qtdq)'
-alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-
 if command -s rsync > /dev/null
 	alias cp="rsync --archive --human-readable --progress --verbose --whole-file"
 else
 	alias cp="cp -v -i"
 end
-
-alias download_song='youtube-dl -f "bestaudio/best" -ciw -o "%(title)s.%(ext)s" --extract-audio --audio-quality 0 --audio-format mp3'
-alias download_video='youtube-dl -f "bestaudio/best" -ciw -o "%(title)s.%(ext)s" --audio-quality 0'
-alias egrep="egrep --color=auto"
-alias fgrep="fgrep --color=auto"
 
 if command -s rg > /dev/null
 	alias grep="rg"
@@ -41,19 +33,10 @@ if command -s exa > /dev/null
 	alias ls="exa -a -l --color=always --group-directories-first"
 	alias lt="exa -a -T --color=always --group-directories-first"
 else
-	alias la="ls -A"
+	alias la="ls -A -h"
+	alias ll="ls -l -h"
+	alias lla="ls -A -l -h"
 end
-
-alias mirrorr="sudo reflector --protocol https --age 6 --latest 50 --number 20 --sort rate --verbose --save /etc/pacman.d/mirrorlist"
-alias mirrors="sudo reflector --protocol https --age 6 --latest 50 --number 20 --sort score --verbose --save /etc/pacman.d/mirrorlist"
-alias mv="mv -v -i"
-alias pacsyu="sudo pacman -Syyu"
-alias pip_env_base_upgrade="python -m pip install --upgrade wheel && python -m pip install --upgrade setuptools && python -m pip install --upgrade pip"
-alias pip_env_req_upgrade='python -m pip install --upgrade --upgrade-strategy "eager" -r'
-alias poweroff="systemctl poweroff"
-alias reboot="systemctl reboot"
-alias rm="rm -v -i"
-alias shutdown="systemctl poweroff"
 
 if command -s nvim > /dev/null
 	alias vi="nvim"
@@ -61,6 +44,30 @@ if command -s nvim > /dev/null
 else
 	alias vi="vim"
 end
+
+alias cleanup='sudo pacman -Rns (pacman -Qtdq)'
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
+alias download_song='youtube-dl -f "bestaudio/best" -ciw -o "%(title)s.%(ext)s" --extract-audio --audio-quality 0 --audio-format mp3'
+alias download_video='youtube-dl -f "bestaudio/best" -ciw -o "%(title)s.%(ext)s" --audio-quality 0'
+
+alias egrep="egrep --color=auto"
+
+alias fgrep="fgrep --color=auto"
+
+alias mirrorr="sudo reflector --protocol https --age 6 --latest 50 --number 20 --sort rate --verbose --save /etc/pacman.d/mirrorlist"
+alias mirrors="sudo reflector --protocol https --age 6 --latest 50 --number 20 --sort score --verbose --save /etc/pacman.d/mirrorlist"
+alias mv="mv -v -i"
+
+alias pacsyu="sudo pacman -Syyu"
+alias pip_env_base_upgrade="python -m pip install --upgrade wheel && python -m pip install --upgrade setuptools && python -m pip install --upgrade pip"
+alias pip_env_req_upgrade='python -m pip install --upgrade --upgrade-strategy "eager" -r'
+alias poweroff="systemctl poweroff"
+
+alias reboot="systemctl reboot"
+alias rm="rm -v -i"
+
+alias shutdown="systemctl poweroff"
 
 ### Starship prompt ###
 if type -q starship
