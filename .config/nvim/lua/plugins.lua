ca0568421b59ecb2dcd25afe01d594a9226fb4ff
ShaharNaveh <>
2021-07-10 20:13:22 +0300

    Save

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,8 +1,14 @@
+local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  vim.api.nvim_command("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
+end
+
 -- vim.cmd [[packadd packer.nvim]]
 
 -- Running "PackerCompile" after every change to this file
 vim.cmd [[autocmd BufWritePost plugins.lua source  | PackerCompile]]
 
+-- local use = require('packer').use
 return require('packer').startup(
 	function(use)
 
@@ -17,6 +23,9 @@ return require('packer').startup(
 		'cespare/vim-toml', 
 		ft={'toml'}
 	}
+
+	use 'neovim/nvim-lspconfig'
+	use 'nvim-lua/completion-nvim'
 
 	end)
 
