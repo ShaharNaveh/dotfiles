9f046dbca8433e72770bfd17c4dad0a78357937d
ShaharNaveh <>
2021-07-21 16:43:21 +0300

    Added more languages

--- .config/nvim/lua/config/plugins/treesitter.lua
+++ .config/nvim/lua/config/plugins/treesitter.lua
@@ -1,5 +1,19 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {'bash', 'css', 'json', 'html', 'lua', 'python', 'toml', 'yaml'},
+  ensure_installed = {
+      'bash',
+      'css',
+      'dockerfile',
+      'fish',
+      'html',
+      'json',
+      'latex',
+      'lua',
+      'python',
+      'regex',
+      'rst',
+      'toml',
+      'yaml'
+  },
   ignore_install = {},  -- List of parsers to ignore installing
   highlight = {
     enable = true,  -- false will disable the whole extension
