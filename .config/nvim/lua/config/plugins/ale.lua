338cc2d422cbce79de3e7cb18d99877f65cd15a4
ShaharNaveh <>
2021-07-24 13:59:20 +0300

    Update ale settings

--- .config/nvim/lua/config/plugins/ale.lua
+++ .config/nvim/lua/config/plugins/ale.lua
@@ -1,13 +1,13 @@
-vim.g.ale_enabled = 1
-vim.g.ale_lint_on_enter = 0
+vim.g.ale_enabled = true
+vim.g.ale_lint_on_enter = false
 vim.g.ale_lint_on_text_changed = "never"
-vim.b.ale_warn_about_trailing_whitespace = 0
+vim.b.ale_warn_about_trailing_whitespace = false
 vim.g.ale_echo_msg_error_str = "E"
 vim.g.ale_echo_msg_warning_str = "W"
-vim.g.ale_echo_msg_format = "[%linter%] %s [%severity%]"
+vim.g.ale_echo_msg_format = '[%linter%] [%severity%] %code: %%s'
 vim.g.ale_completion_enabled = false
-vim.g.ale_set_highlights = 0
-vim.g.ale_fix_on_save = 1
+vim.g.ale_set_highlights = false
+vim.g.ale_fix_on_save = true
 
 vim.g.ale_linters = {
     ["*"] = { 'remove_trailing_lines', 'trim_whitespace' },
