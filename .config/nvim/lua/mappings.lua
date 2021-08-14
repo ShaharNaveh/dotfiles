cf1dd45ad35c8d72b2c8bdc68df92129ab3964ab
ShaharNaveh <>
2021-08-14 18:23:06 +0300

    Moved keymappings

---
+++ .config/nvim/lua/mappings.lua
@@ -0,0 +1,35 @@
+local function map(mode, lhs, rhs, opts)
+    local options = {noremap = true, silent = true}
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+local cmd = vim.cmd
+local opt = {}
+
+vim.g.mapleader = " "
+
+-- [[ Toggle numbers
+-- TODO: Fix with relativenumbers
+map("n", "<leader>n", [[ <Cmd> set nu!<CR>]], opt)
+-- ]]
+
+-- [[ <C-{hjkl}> to navigate splits
+map('n', '<c-k>', [[<Cmd>wincmd k<CR>]], opt)
+map('n', '<c-j>', [[<Cmd>wincmd j<CR>]], opt)
+map('n', '<c-h>', [[<Cmd>wincmd h<CR>]], opt)
+map('n', '<c-l>', [[<Cmd>wincmd l<CR>]], opt)
+-- ]]
+
+-- [[ NvimTree
+map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", opts)
+-- ]]
+
+-- Packer commands till because we are not loading it at startup
+vim.cmd("silent! command PackerCompile lua require 'plugin_list' require('packer').compile()")
+vim.cmd("silent! command PackerInstall lua require 'plugin_list' require('packer').install()")
+vim.cmd("silent! command PackerStatus lua require 'plugin_list' require('packer').status()")
+vim.cmd("silent! command PackerSync lua require 'plugin_list' require('packer').sync()")
+vim.cmd("silent! command PackerUpdate lua require 'plugin_list' require('packer').update()")
