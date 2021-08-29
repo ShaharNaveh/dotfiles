7024aee6546fad3fcfdd56d67bc0138b2f657303
ShaharNaveh <>
2021-08-29 17:33:16 +0300

    Set colorscheme as nightfox

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,12 +3,41 @@ set fish_greeting
 set TERM "screen-256color"
 
 ### AUTOCOMPLETE AND HIGHLIGHT COLORS ###
-set fish_color_normal brcyan
-set fish_color_autosuggestion '#7d7d7d'
-set fish_color_command brcyan
-set fish_color_error '#ff6c6b'
-set fish_color_param brcyan
+# Nightfox Color Palette
+# Style: nightfox
+# Upstream: https://github.com/edeneast/nightfox.nvim/raw/main/extra/nightfox/nightfox_fish.fish
+set -l foreground cdcecf
+set -l selection 283648
+set -l comment 526175
+set -l red c94f6d
+set -l orange f4a261
+set -l yellow dbc074
+set -l green 81b29a
+set -l magenta 9d79d6
+set -l cyan 63cdcf
+set -l pink d67ad2
 
+# Syntax Highlighting Colors
+set -g fish_color_normal $foreground
+set -g fish_color_command $cyan
+set -g fish_color_keyword $pink
+set -g fish_color_quote $yellow
+set -g fish_color_redirection $foreground
+set -g fish_color_end $orange
+set -g fish_color_error $red
+set -g fish_color_param $purple
+set -g fish_color_comment $comment
+set -g fish_color_selection --background=$selection
+set -g fish_color_search_match --background=$selection
+set -g fish_color_operator $green
+set -g fish_color_escape $pink
+set -g fish_color_autosuggestion $comment
+
+# Completion Pager Colors
+set -g fish_pager_color_progress $comment
+set -g fish_pager_color_prefix $cyan
+set -g fish_pager_color_completion $foreground
+set -g fish_pager_color_description $comment
 
 source "$HOME/.config/fish/functions/builtins.fish"
 source "$HOME/.config/fish/functions/aliases.fish" # Load aliases
