d9b6dadf06039abab07827b0cc9d8c1f41f1caff
ShaharNaveh <>
2021-08-30 14:41:18 +0300

    Apply stylua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,10 @@
-
 local main_config = {
 	"options",
 	"mappings",
-    "theme",
+	"theme",
 	"utils",
 }
 
 for _, config_file in ipairs(main_config) do
-    pcall(require, config_file)
- end
+	pcall(require, config_file)
+end
