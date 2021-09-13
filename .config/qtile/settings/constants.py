61f2250db46b6123b42cdba62708f2736c7aafb0
ShaharNaveh <>
2021-09-13 16:25:10 +0300

    Refactored qtile with nord

--- .config/qtile/settings/constants.py
+++ .config/qtile/settings/constants.py
@@ -1,9 +1,16 @@
 import pathlib
 
 browser = "firefox"
-font = "DroidSansMono Nerd Font"
+font = "FiraCode Nerd Font"
 mod = "mod4"
 terminal = "alacritty"
-theme = "gruvbox"
+theme = "custom"
+
+wallpaper = (
+    pathlib.Path.home()
+    / "Wallpapers"
+    / "Unknown-Husky_Walks_Across_A_Frozen_Lake_After_Rain_1.jpg"
+)
+
 
 qtile_config_dir = pathlib.Path.home() / ".config" / "qtile"
