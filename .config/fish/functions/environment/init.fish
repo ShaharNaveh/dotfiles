b32c02aa7a6ee99c4f7d7c2ecfb353a4ac543212
Shahar Naveh <50263213+ShaharNaveh@users.noreply.github.com>
2022-11-29 11:26:45 +0200

    Save

--- .config/fish/functions/environment/init.fish
+++ .config/fish/functions/environment/init.fish
@@ -1,3 +1,4 @@
-source "$HOME/.config/fish/functions/environment/npm.fish"
 source "$HOME/.config/fish/functions/environment/firefox.fish"
+source "$HOME/.config/fish/functions/environment/npm.fish"
+source "$HOME/.config/fish/functions/environment/terraform.fish"
 source "$HOME/.config/fish/functions/environment/xdg.fish"
---
+++ .config/fish/functions/environment/terraform.fish
@@ -0,0 +1,1 @@
+set -Ux TF_PLUGIN_CACHE_DIR "$HOME/.cache/terraform/plugin-cache"
